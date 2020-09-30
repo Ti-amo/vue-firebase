53aa94b9dfc6acb2cc401cc39b8b9668a493c77e
Ti-amo <thehungklk@gmail.com>
2020-09-30 14:15:48 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-// firebase init
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
-}
-firebase.initializeApp(firebaseConfig)
+  apiKey: "AIzaSyAGP_1dIZtKqpyiVUOi9dMi3dA94wsPhz8",
+  authDomain: "electric-report-da493.firebaseapp.com",
+  databaseURL: "https://electric-report-da493.firebaseio.com",
+  projectId: "electric-report-da493",
+  storageBucket: "electric-report-da493.appspot.com",
+  messagingSenderId: "537583038916",
+  appId: "1:537583038916:web:3c53ab6efd431d5a957491",
+  measurementId: "G-K8JCEXR9TV"
+};
+
+  firebase.initializeApp(firebaseConfig)
 
 // utils
 const db = firebase.firestore()
